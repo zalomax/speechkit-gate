e652623389d99574def63a793d4bdbbb920ee15f
Valentin Tomashevski <srehilie@gmail.com>
2019-04-12 15:55:30 +0300

    added env file v0.1

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const keys_dir = '/etc/ssl/auth.mmkc.su/'
 const fs = require('fs')
 const https = require('https')
 console.log('environment:', process.env.NODE_ENV)
+const server_options = {}
 if (process.env.NODE_ENV !== 'development') {
-    const server_options = {
-        key: fs.readFileSync(keys_dir + 'key.pem'),
-        ca: fs.readFileSync(keys_dir + 'chain.pem'),
-        cert: fs.readFileSync(keys_dir + 'chain.pem')
-    }
+    server_options.key = fs.readFileSync(keys_dir + 'key.pem')
+    server_options.ca = fs.readFileSync(keys_dir + 'chain.pem')
+    server_options.cert = fs.readFileSync(keys_dir + 'chain.pem')
 }
 
 app.use(cors())
