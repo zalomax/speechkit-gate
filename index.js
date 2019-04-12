566810e2180ad71184acf4bda23b64954a9f884e
Valentin Tomashevski <srehilie@gmail.com>
2019-04-12 14:30:51 +0300

    added CORS

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const router = require('./config/router')
 const bodyParser = require('body-parser');
+const cors = require('cors')
+// const server_options = {
+//     key: fs.readFileSync(keys_dir + 'privatekey.pem'),
+//     ca: fs.readFileSync(keys_dir + 'certauthority.pem'),
+//     cert: fs.readFileSync(keys_dir + 'certificate.pem')
+// }
 
+app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send("Simple API Gateway")
