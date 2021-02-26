4e7362426300c3b60ddc0b33cbc11be9d90480d0
Maxim Kostukov <6357654@gmail.com>
2021-02-26 13:11:32 +0300

    fix certs

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const router = require('./config/router')
 const bodyParser = require('body-parser');
 const cors = require('cors')
 require('dotenv').config()
-const keys_dir = '/etc/ssl/auth.mmkc.su/'
+const keys_dir = '/etc/letsencrypt/live/auth.mmkc.su/'
 const fs = require('fs')
 const https = require('https')
 console.log('environment:', process.env.NODE_ENV)
 const server_options = {}
 if (process.env.NODE_ENV !== 'development') {
-    server_options.key = fs.readFileSync(keys_dir + 'key.pem')
-    server_options.ca = fs.readFileSync(keys_dir + 'chain.pem')
-    server_options.cert = fs.readFileSync(keys_dir + 'chain.pem')
+    server_options.key = fs.readFileSync(keys_dir + 'privkey.pem')
+    server_options.ca = fs.readFileSync(keys_dir + 'fullchain.pem')
+    server_options.cert = fs.readFileSync(keys_dir + 'cert.pem')
 }
 
 app.use(cors())
